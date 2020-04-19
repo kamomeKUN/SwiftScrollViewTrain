e6ed29d67d2a7b1135f3804817d161ca08d81abd
kamomeKUN <con67vint@gmail.com>
2020-04-19 18:53:15 +0900

    [mod] スクロールビューをコードで書く方法を適応
    コード版

--- Swift5ScrollViewTrain/ViewController.swift
+++ Swift5ScrollViewTrain/ViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 画面のUIを設定
+        configureSV()
+    }
+    
+    func createContents() -> UIView {
+        
+        let contentsView = UIView()
+        // 位置、サイズの設定
+        contentsView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 1200)
+        
+        let label = createLabel(contentsView: contentsView)
+        contentsView.addSubview(label)
+        
+        return contentsView
+        
+    }
+    
+    func createLabel(contentsView: UIView) -> UILabel {
+        
+        let label = UILabel()
+        
+        // 位置、大きさを設定
+        label.frame.size = CGSize(width: 255, height: 50)
+        label.center = contentsView.center
+        label.text = "StayHome"
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        label.textAlignment = NSTextAlignment.center
+        
+        return label
+    }
+    
+    func configureSV() {
+        
+        let subView = createContents()
+        scrollView.addSubview(subView)
+        
+        scrollView.contentSize = subView.frame.size
     }
 
 
