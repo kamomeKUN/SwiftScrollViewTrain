9e8efe4c358b67ef7079fa2fea37d74400712bb6
kamomeKUN <con67vint@gmail.com>
2020-04-19 17:49:50 +0900

    first commit
    StoryBoard版

---
+++ Swift5ScrollViewTrain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5ScrollViewTrain
+//
+//  Created by kamomeKUN on 2020/04/19.
+// Copyright ©︎ 2020 kamomeKUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
